df6b94596ac634f7c4efed2028ac5071c8624a77
RubenGonz <ruben30303030@gmail.com>
2021-05-06 17:19:01 +0100

    #7 Cambio del modo_evoluciona

--- Pokemon/sql/BaseDeDatos.sql
+++ Pokemon/sql/BaseDeDatos.sql
@@ -59,7 +59,7 @@ CREATE TABLE POKEMON (
 CREATE TABLE EVOLUCIONA (
     numero_pokedex_origen INT CHECK (numero_pokedex_origen > 0),
     numero_pokedex_destino INT CHECK (numero_pokedex_destino > 0),
-    modo_evoluciona VARCHAR (20) CHECK (modo_evoluciona IN ("Nivel", "Intercambio", "Objeto")),
+    modo_evoluciona VARCHAR (20) CHECK (modo_evoluciona IN ("Nivel", "Intercambio", "Piedra")),
     PRIMARY KEY (numero_pokedex_origen, numero_pokedex_destino),
     FOREIGN KEY (numero_pokedex_origen) REFERENCES POKEMON (numero_pokedex),
     FOREIGN KEY (numero_pokedex_destino) REFERENCES POKEMON (numero_pokedex)
@@ -109,7 +109,7 @@ CREATE TABLE ESTADO (
 CREATE TABLE OBJETO (
     id_objeto INT CHECK (id_objeto > 0),
     nombre VARCHAR (25),
-    modo_obtencion VARCHAR (20) CHECK (modo_obtencion IN ("Comprado", "Recogido", "Entregado", "Quitado")),
+    modo_obtencion VARCHAR (20) CHECK (modo_obtencion IN ("Comprado", "Recogido", "Entregado")),
     precio_venta INT CHECK (precio_venta > 0),
     PRIMARY KEY (id_objeto)
 );
