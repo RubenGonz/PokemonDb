d2177e45058eecd3c0e412f509c512c97add620e
RubenGonz <ruben30303030@gmail.com>
2021-05-06 01:16:55 +0100

    #11 Incorporacion de los estados

--- Pokemon/sql/Script.sql
+++ Pokemon/sql/Script.sql
@@ -30,6 +30,17 @@
  * ESTADOS
 */
 
+INSERT INTO ESTADO VALUES (1,"Paralizado",1,"Puede evitar que el pokemon ataque");
+INSERT INTO ESTADO VALUES (2,"Quemado",1,"Daña al pokemon cada ronda");
+INSERT INTO ESTADO VALUES (3,"Envenenado",1,"Daña al pokemon cada ronda exponencialmente");
+INSERT INTO ESTADO VALUES (4,"Dormido",1,"Duerme al pokemon");
+INSERT INTO ESTADO VALUES (5,"Congelado",1,"Puede evitar que el pokemon ataque");
+INSERT INTO ESTADO VALUES (6,"Confuso",2,"Puede evitar que el pokemon ataque y que se haga daño");
+INSERT INTO ESTADO VALUES (7,"Enamorado",2,"Puede evitar que el pokemon ataque");
+INSERT INTO ESTADO VALUES (8,"Drenado",2,"Roba vida del pokemon y se la da al atacante");
+INSERT INTO ESTADO VALUES (9,"Maldito",2,"Daña al pokemon");
+INSERT INTO ESTADO VALUES (5,"Atrapado",2,"Evita que el pokemon huya");
+
 /**
  * ENTRENADORES
 */
