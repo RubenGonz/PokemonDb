71465d58d05bedff820550842010b9c61ebface0
Joseman2000LR <jose12linares@gmail.com>
2021-05-14 12:08:36 +0100

    #4  cambios en PokemonException

--- Pokemon/src/main/java/es/iespuertolacruz/pokemon/exception/PokemonException.java
+++ Pokemon/src/main/java/es/iespuertolacruz/pokemon/exception/PokemonException.java
@@ -1,5 +1,16 @@
 package es.iespuertolacruz.pokemon.exception;
 
-public class PokemonException {
+
+
+public class PokemonException  extends Exception{
+     
+    public PokemonException(String mensaje){
+        super(mensaje);
+    }
+
+    public PokemonException(String mensaje ,Exception causa) {
+        super(mensaje , causa);
+    }
+
     
 }
