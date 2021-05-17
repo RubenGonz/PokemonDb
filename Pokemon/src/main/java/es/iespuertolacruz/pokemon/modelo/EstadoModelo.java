3efcaf4d50cd7ee8e656bcec21b2eaaacb79caba
Joseman2000LR <jose12linares@gmail.com>
2021-05-18 00:20:01 +0100

    #14 cambios en EstadoModelo

--- Pokemon/src/main/java/es/iespuertolacruz/pokemon/modelo/EstadoModelo.java
+++ Pokemon/src/main/java/es/iespuertolacruz/pokemon/modelo/EstadoModelo.java
@@ -20,7 +20,7 @@ public class EstadoModelo {
     }
 
     public void insertar(Estado estado) throws PersistenciaException {
-        persistencia.insertar(estado);
+        persistencia.insertarEstado(estado);
     }
 
     public void eliminar(Estado estado) throws PersistenciaException {
